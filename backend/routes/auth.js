bb267df7a9e00dfb77ac478002b54f594c7d5a51
D-E-V-Shukla <shuklaharsh5840@gmail.com>
2025-05-02 13:07:31 +0530

    schema changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,16 @@
 const authrouter = require('express').Router();
-
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient;
 
 
 authrouter.post('/login', (req,res)=>{
     //login logic here
 });
+
+authrouter.post('/register', (req,res)=>{
+    const {email , password} = req.body;
+    const user = prisma.user.crea
+})
 
 
 module.exports = authrouter;
